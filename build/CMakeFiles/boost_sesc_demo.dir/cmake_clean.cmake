8c8c6d5c04aacfd07d6bc31dca4b21484fd40f05
yjqpro <yjqpro@gmail.com>
2018-05-16 06:17:57 +0800

    impl

---
+++ build/CMakeFiles/boost_sesc_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_sesc_demo.dir/main.cc.o"
+  "boost_sesc_demo.pdb"
+  "boost_sesc_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_sesc_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <thread>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace std::placeholders;
+
+struct Data {
+  Data(int i) : i_(i) {}
+  Data(const Data& other) {
+    std::cout << "copy constract" << std::endl;
+    i_ = other.i_;
+  }
+
+  Data(Data&& other) = default;
+  
+  friend std::ostream& operator<<(std::ostream& os, const Data& data) {
+    return os << data.i_ << std::endl;
+  }
+
+  int i_;
+};
+
+using LockFreeQueue = boost::lockfree::spsc_queue<Data,boost::lockfree::capacity<1024>,
+    boost::lockfree::fixed_sized<false> >;
+typedef typename LockFreeQueue::value_type QueueItemType;
+
+
+void Processer(const QueueItemType& msg) {
+  std::cout << msg << std::endl;
+}
+
+void Consumer(LockFreeQueue& queue, bool& stoped) {
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  while (!stoped) {
+    queue.consume_all(std::bind(&Processer, _1));
+  }
+}
+
+
+int main(int argc, char** argv) {
+   LockFreeQueue queue;
+   bool stoped = false;
+
+  auto thrd = std::thread(std::bind(&Consumer, std::ref(queue), std::ref(stoped)));
+
+
+  std::string input;
+  while (std::cin >> input) {
+    if (input=="exit") {
+      stoped = true;
+      break;
+    }
+    // queue.push(input);
+    std::cout << "perpare push" << std::endl;
+    queue.push(Data{boost::lexical_cast<int>(input)});
+    std::cout << "after push" << std::endl;
+  }
+
+  thrd.join();
+  return 0;
+}
+
